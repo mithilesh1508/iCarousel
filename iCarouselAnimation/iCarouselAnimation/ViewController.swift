07c7d1b0cd212399590aace19e56cd2228fa7d5a
Mithilesh Kumar <mithilesh.kmr786@gmail.com>
2022-04-16 01:07:55 +0530

    Initial Import

---
+++ iCarouselAnimation/iCarouselAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iCarouselAnimation
+//
+//  Created by Mithilesh Kumar on 16/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
